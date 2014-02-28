170fdf7d140aeda3b08f03fa171c91a3397b87b6
Boinx Bantug <boinxbantug25@yahoo.com>
2014-02-28 15:16:35 +0800

    B

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -1,6 +1,132 @@
-$(document).ready(function (){
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
 
-  var menu = "close";
+        playAudio("/android_asset/HighScore.mp3");
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+// Audio player
+//
+var my_media = null;
+var mediaTimer = null;
+
+// Play audio
+//
+function playAudio(src) {
+    // Create Media object from src
+    my_media = new Media(src, onSuccess, onError);
+
+    // Play audio
+    my_media.play();
+
+    // Update my_media position every second
+    if (mediaTimer == null) {
+        mediaTimer = setInterval(function() {
+            // get my_media position
+            my_media.getCurrentPosition(
+                // success callback
+                function(position) {
+                    if (position > -1) {
+                        setAudioPosition((position) + " sec");
+                    }
+                },
+                // error callback
+                function(e) {
+                    console.log("Error getting pos=" + e);
+                    setAudioPosition("Error: " + e);
+                }
+            );
+        }, 1000);
+    }
+}
+
+// Pause audio
+// 
+function pauseAudio() {
+    if (my_media) {
+        my_media.pause();
+    }
+}
+
+// Stop audio
+// 
+function stopAudio() {
+    if (my_media) {
+        my_media.stop();
+    }
+    clearInterval(mediaTimer);
+    mediaTimer = null;
+}
+
+// onSuccess Callback
+//
+function onSuccess() {
+    console.log("playAudio():Audio Success");
+}
+
+// onError Callback 
+//
+function onError(error) {
+    alert('code: '    + error.code    + '\n' + 
+          'message: ' + error.message + '\n');
+}
+
+// Set audio position
+// 
+function setAudioPosition(position) {
+    document.getElementById('audio_position').innerHTML = position;
+}
+
+
+$(document).ready(function (){
+    
+    app.initialize();
+  	var menu = "close";
 	var name = " ";
 	var storedName = localStorage.getItem('userName');
 	var topScoreQuiz1 = localStorage.getItem('highScoreQuiz1');
@@ -14,7 +140,8 @@ $(document).ready(function (){
 	var AnswersModule2 = [];
 	var AnswersModule3 = [];
 	$('#logOut').hide();
-	var HighScoreSound = new Media('HighScore.mp3');
+
+	var HighScoreSound = new Media('HighScore.mp3'); 
 	// var HighScoreSound = document.createElement('audio');
  //    HighScoreSound.setAttribute('src', 'HighScore.mp3');
     
@@ -75,6 +202,7 @@ $(document).ready(function (){
 	$('#history').click(function(){
 		$.ajax({
 			url: "history.html",
+			transtion:'slide',
 			success:function(result){
 				$('.con').html(result);
 				sideBarTransition();
@@ -147,7 +275,6 @@ $(document).ready(function (){
 				$(".con").html(result);
 			}});
 	});
-
 
 	// bind
 	function bindQuizIntroEvents(){
@@ -279,17 +406,6 @@ $(document).ready(function (){
 		});
 	}
 
-	//Exit Button Modal in Quiz-Done
-	function exitButtonModal(){
-		$('#exitBut').click(function(){
-
-			$('#topScoreWindow').hide();
-			$('.modal-backdrop').hide();
-
-		});
-
-	}
-
 	//Module 1 Button
 	function bindButton1(){
 	$('#button1').click(function(){
@@ -418,7 +534,6 @@ $(document).ready(function (){
 								$(".topScore").html(highScorer + ":" +topScoreQuiz1);
 								$('#topScoreWindow').modal().fadeIn(1000);
 								playSound();
-								exitButtonModal();
 								reviewAnswers(AnswersModule1);
 								retakeQuiz1();
 								// alert('New High Score!' + " " + topScoreQuiz1);
@@ -475,7 +590,6 @@ $(document).ready(function (){
 							$(".topScore").html(highScorer + ":" +topScoreQuiz2);
 							$('#topScoreWindow').modal('show');
 							playSound();
-							exitButtonModal();
 							reviewAnswers(AnswersModule2);
 							retakeQuiz2();
 							// alert('New High Score!' + " " + topScoreQuiz2);
@@ -530,7 +644,6 @@ $(document).ready(function (){
 							$(".topScore").html(highScorer + ":" +topScoreQuiz3);
 							$('#topScoreWindow').modal('show');
 							playSound();
-							exitButtonModal();
 							reviewAnswers(AnswersModule3);
 							retakeQuiz3();
 							// alert('New High Score!' + " " + topScoreQuiz3);
